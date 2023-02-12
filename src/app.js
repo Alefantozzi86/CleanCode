782f35bb267cb380e50ee691e2491bbe3c834039
Alefantozzi86 <alefantozzi86@gmail.com>
2023-02-12 21:19:40 +0000

    proyectolisto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function randomNumber(arrayLenght) {
+  return Math.floor(Math.random() * arrayLenght);
+}
+function getWho() {
+  const who = ["The dog", "My grandma", "His turtle", "My bird", "My cat"];
+  return who[randomNumber(who.length)];
+}
+function getAction() {
+  const action = ["ate", "peed", "crushed", "broke"];
+  return action[randomNumber(action.length)];
+}
+function getWhat() {
+  const what = ["my homework", "the keys", "the car", "the food", "the book"];
+  return what[randomNumber(what.length)];
+}
+function getWhen() {
+  const when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  return when[randomNumber(when.length)];
+}
+function alert(Excusa) {
+  document.getElementById("Excusa").innerHTML = Excusa.join(" ");
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generador() {
+  let respuesta = [];
+  respuesta.push(getWho());
+  respuesta.push(getAction());
+  respuesta.push(getWhat());
+  respuesta.push(getWhen());
+  alert(respuesta);
+}
